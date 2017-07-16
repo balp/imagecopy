cba7ea8b0a854fb99bbcd02cb09eeecdbfff2e60
Anders Arnholm <anders@arnholm.se>
2017-07-16 07:08:40 +0200

    Add licens clarification

--- image_import.py
+++ image_import.py
@@ -8,66 +8,78 @@ import filecmp
 import sys
 import shutil
 
-def importfiles(arg, dirname, fnames):
-    for fname in fnames:
-        destpath = None
-        srcfile = os.path.join(dirname, fname)
-        created_time = time.gmtime(os.stat(srcfile).st_ctime)
-        (banename, ext) = os.path.splitext(fname)
-        if ext.lower() == ".jpg":
-            destpath = os.path.join(arg,
-                    "%04d" % (created_time.tm_year),
-                    "%02d" % (created_time.tm_mon),
-                    "%02d" % (created_time.tm_mday),
-                    )
-        elif ext.lower() == ".mov":
-            destpath = os.path.join(arg,
-                    "%04d" % (created_time.tm_year),
-                    "%02d" % (created_time.tm_mon),
-                    "%02d" % (created_time.tm_mday),
-                    "movies"
-                    )
-        elif ext.lower() == ".cr2":
-            destpath = os.path.join(arg,
-                    "%04d" % (created_time.tm_year),
-                    "%02d" % (created_time.tm_mon),
-                    "%02d" % (created_time.tm_mday),
-                    "CR2"
-                    )
-        if not destpath:
-            return
-        if not os.path.exists(destpath):
-            os.makedirs(destpath)
-        if not os.path.isdir(destpath):
-            return
-        srcfile = os.path.join(dirname, fname)
-        destfile = os.path.join(destpath, fname.lower())
-        if os.path.exists(destfile):
-            cnt = 0;
-            if filecmp.cmp(srcfile, destfile, shallow=False): # We have the file, don't copy
-                print "have: %s as %s" % (srcfile, destfile)
-                destfile = None
-            else:
-                print "diff: %s != %s" % (srcfile, destfile)
-                while True:
-                    newname = "%s-%d%s" % (banename, cnt, ext)
-                    destfile = os.path.join(destpath, newname.lower() )
-                    cnt += 1
-                    if not os.path.exists(destfile):
-                        break
-                    if filecmp.cmp(srcfile, destfile, shallow=False):
-                        print "have: %s as %s" % ( srcfile, destfile )
-                        destfile = None
-                        break
+def get_destination_path(cachepath, srcfile):
+    destpath = None
+    created_time = time.gmtime(os.stat(srcfile).st_ctime)
+    (basename, ext) = os.path.splitext(fname)
+    if ext.lower() == ".jpg":
+        destpath = os.path.join(cachepath,
+                "%04d" % (created_time.tm_year),
+                "%02d" % (created_time.tm_mon),
+                "%02d" % (created_time.tm_mday),
+                )
+    elif ext.lower() == ".mov":
+        destpath = os.path.join(cachepath,
+                "%04d" % (created_time.tm_year),
+                "%02d" % (created_time.tm_mon),
+                "%02d" % (created_time.tm_mday),
+                "movies"
+                )
+    elif ext.lower() == ".cr2":
+        destpath = os.path.join(cachepath,
+                "%04d" % (created_time.tm_year),
+                "%02d" % (created_time.tm_mon),
+                "%02d" % (created_time.tm_mday),
+                "CR2"
+                )
+    return destpath
+
+def get_destination_filename(destpath, fname, srcfile):
+    destfile = os.path.join(destpath, fname.lower())
+    print("To: ", destfile, destpath, fname)
+    if os.path.exists(destfile):
+        cnt = 0;
+        if filecmp.cmp(srcfile, destfile, shallow=False): # We have the file, don't copy
+            print("have: %s as %s" % (srcfile, destfile))
+            destfile = None
+        else:
+            print("diff: %s != %s" % (srcfile, destfile))
+            while True:
+                (basename, ext) = os.path.splitext(fname)
+                newname = "%s-%d%s" % (basename, cnt, ext)
+                destfile = os.path.join(destpath, newname.lower() )
+                cnt += 1
+                if not os.path.exists(destfile):
+                    break
+                if filecmp.cmp(srcfile, destfile, shallow=False):
+                    print("have: %s as %s" % ( srcfile, destfile ))
+                    destfile = None
+                    break
+    return destfile
 
 
-        if destfile:
-            print "copy: %s -> %s" % (srcfile, destfile)
-            shutil.copy2(srcfile, destfile)
+def importfile(cachepath, dirname, fname):
+    srcfile = os.path.join(dirname, fname)
+    destpath = get_destination_path(cachepath, srcfile)
+    if not destpath:
+        return
+    if not os.path.exists(destpath):
+        os.makedirs(destpath)
+    if not os.path.isdir(destpath):
+        return
+    destfile = get_destination_filename(destpath, fname, srcfile)
+
+    if destfile:
+        print("copy: %s -> %s" % (srcfile, destfile))
+        shutil.copy2(srcfile, destfile)
+    else:
+        print("no copy: %s -> %s" % (srcfile, destfile))
 
 if __name__ == "__main__":
     cachepath = "P:\\"
     cards = [ "g:\\DCIM", "/Volumes/NO NAME", "/Volumes/EOS_DIGITAL/" ] 
 
     for card in cards:
-        os.path.walk( card, importfiles, cachepath)
+        for dirName, subdirlist, fileList in os.walk( card ):
+            for fname in fileList:
+                importfile(cachepath, dirName, fname)
